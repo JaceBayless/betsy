cc6f065ede4a6b9cceb15df768da3d2480a35012
Jace Bayless <jacebayless@gmail.com>
2021-09-07 03:01:49 +0000

    Cleaned up spec

--- spec/betsy/shop_listing_file_spec.rb
+++ spec/betsy/shop_listing_file_spec.rb
@@ -7,7 +7,9 @@ describe Betsy::ShopListingFile do
       stub_request(:delete, "https://openapi.etsy.com/v3/application/shops/1/listings/1/files/1")
       Betsy::ShopListingFile.delete_listing_file(1, 1, 1)
     end
+  end
 
+  describe "get_listing_file" do
     it "returns a listing file" do
       stub_request(:get, "https://openapi.etsy.com/v3/application/shops/1/listings/1/files/1")
         .to_return(body: SINGLE_SHOP_LISTING_FILE)
@@ -22,7 +24,9 @@ describe Betsy::ShopListingFile do
       expect(listing_file.filetype).to eq "string"
       expect(listing_file.create_timestamp).to eq 946684800
     end
+  end
 
+  describe "get_all_listing_files" do
     it "returns all listing files for a listing" do
       stub_request(:get, "https://openapi.etsy.com/v3/application/shops/1/listings/1/files")
         .to_return(body: MULTIPLE_SHOP_LISTING_FILES)
@@ -32,7 +36,9 @@ describe Betsy::ShopListingFile do
       expect(listing_files.first).to be_kind_of Betsy::ShopListingFile
       expect(listing_files.first.listing_file_id).to eq 1
     end
+  end
 
+  describe "upload_listing_file" do
     it "uploads a listing file" do
       stub_request(:post, "https://openapi.etsy.com/v3/application/shops/1/listings/1/files")
         .to_return(body: SINGLE_SHOP_LISTING_FILE)
